7297d28ba8bbb177ddc9160c137ab2267823bf6e
irons163 <irons@ironsdeMacBook-Air.local>
2015-04-23 09:45:10 +0800

    Initial Commit

---
+++ Try_rabbit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Try_rabbit
+//
+//  Created by irons on 2015/4/23.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Try_rabbitTests/Try_rabbitTests.m
@@ -0,0 +1,34 @@
+//
+//  Try_rabbitTests.m
+//  Try_rabbitTests
+//
+//  Created by irons on 2015/4/23.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Try_rabbitTests : XCTestCase
+
+@end
+
+@implementation Try_rabbitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
